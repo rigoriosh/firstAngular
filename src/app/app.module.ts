f5574b6a93e8599efe4fd7b5429d2a8a4fbca735
rigoriosh <rigoriosh@gmail.com>
2020-12-22 05:37:59 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.comp';
+import { bodyComponent } from './components/body/body.componet';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, HeaderComponent, bodyComponent, FooterComponent
   ],
   imports: [
     BrowserModule
