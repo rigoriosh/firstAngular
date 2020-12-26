836fb50d9ff8afb708df1705b1a8af9731b75267
rigoriosh <rigoriosh@gmail.com>
2020-12-26 10:18:21 -0500

    0.1Commit

--- src/app/components/body/body.componet.ts
+++ src/app/components/body/body.componet.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
     templateUrl:'./body.component.html'
 })
 export class bodyComponent{
-
+    frase: any = {
+        mensaje:'Un gran poder requiere una gran responsabilidad',
+        autor: 'Ben Parker'
+    }
+    mostrar: boolean = true;
+    personajes: string[] = ['Spiderman', 'Venom', 'Dr. Octopus']
 }
